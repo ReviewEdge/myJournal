b847b5dfd944cc104e7f0e78bec686c9acad457d
RICHARDSONLG20 <RICHARDSONLG20@10.37.103.103>
2020-11-13 16:16:39 -0500

    created pages

--- src/myJournal/DataStructures/Page.java
+++ src/myJournal/DataStructures/Page.java
@@ -2,9 +2,35 @@ package myJournal.DataStructures;
 
 public abstract class Page {
 	private final int id;
-	private int likes;
-	private int views;
+	private String name;
 	private String content;
 	
+	public Page(int newId) {
 	
+	}
+	
+	public int getId {
+		
+	}
+	
+	public String getContent {
+		
+	}
+	
+	public int getLikes {
+		
+	}
+	
+	
+	public int getViews {
+		
+	}
+	
+	public void setName {
+		
+	}
+	
+	public void addLike {
+		
+	}
 }
