a83ee35185b64152f5f2a7938a36201ede1c4d6c
BROWDERKD20 <BROWDERKD20@gcc.edu>
2020-11-15 21:30:03 -0500

    Add getters, setters, and javadoc for AccountData.

--- src/myJournal/DataStructures/AccountData.java
+++ src/myJournal/DataStructures/AccountData.java
@@ -13,4 +13,108 @@ public class AccountData {
     private Date dateOfBirth;
     private String bio;
     private String livingLocation;
+
+    /**
+     *
+     * @return the first name.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     *
+     * @param firstName new first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     *
+     * @param lastName the new last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     *
+     * @return the account's username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     *
+     * @param username the new username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     *
+     * @return the date of account creation
+     */
+    public Date getAccountCreation() {
+        return accountCreation;
+    }
+
+    /**
+     *
+     * @return the date of birth
+     */
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     *
+     * @param dateOfBirth the new date of birth
+     */
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     *
+     * @return the profile's bio information
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     *
+     * @param bio new bio information
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    /**
+     *
+     * @return where the person lives
+     */
+    public String getLivingLocation() {
+        return livingLocation;
+    }
+
+    /**
+     *
+     * @param livingLocation where the person lives
+     */
+    public void setLivingLocation(String livingLocation) {
+        this.livingLocation = livingLocation;
+    }
 }
