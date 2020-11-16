b892bac23e16d4add3e46cb8f998a47cecde0eb1
BROWDERKD20 <BROWDERKD20@gcc.edu>
2020-11-15 20:54:03 -0500

    Start adding javadoc

--- src/myJournal/DataStructures/Followable.java
+++ src/myJournal/DataStructures/Followable.java
@@ -5,38 +5,38 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
- *
+ * Classes that implement followable can have their pages shown to follower classes.
  */
 public interface Followable {
     /**
      *
-     * @return
+     * @return the number of followers a followable object has.
      */
     HashSet<Long> getFollowers();
 
     /**
-     *
-     * @param requestingId
-     * @return
+     * Based on the permissions of the requesting account, gives a list of pages available to that account.
+     * @param requestingId The id of the account requesting pages from the followable object.
+     * @return An arraylist of pages accessible to the user from the followable object.
      */
     ArrayList<Page> getPages(long requestingId);
 
     /**
-     *
-     * @param requestingId
-     * @return
+     * Based on the permissions of the requesting account, gives the latest page avaiable to that account.
+     * @param requestingId The id of the account requesting pages from the followable object.
+     * @return The latest page accessible to the user.
      */
     Page getLatestPage(long requestingId);
 
     /**
      *
-     * @return
+     * @return the number of followers the followable object has.
      */
     long getNumFollowers();
 
     /**
      *
-     * @return
+     * @return the number of pages the followable object has.
      */
     long getNumPages();
 }
--- src/myJournal/Main.java
+++ src/myJournal/Main.java
@@ -1,5 +1,8 @@
 package myJournal;
 
+/**
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
