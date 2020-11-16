ab4718653a058fe7203db565e5d5760fa126cb95
RICHARDSONLG20 <RICHARDSONLG20@5CG024DDYK.gcc.edu>
2020-11-15 20:37:22 -0500

    added page javadoc

--- src/myJournal/DataStructures/Page.java
+++ src/myJournal/DataStructures/Page.java
@@ -21,70 +21,128 @@ public class Page {
 		this.parentJournal = parentJournal;
 	}
 	
+	/**
+	 * @return id of page
+	 */
 	public long getId() {
 		return this.id;
 	}
 	
+	/**
+	 * @return the name of the page
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * @return the page's text content
+	 */
 	public String getContent() {
 		return this.content;
 	}
 	
+	/**
+	 * @return the page author's id
+	 */
 	public long getAuthorId() {
 		return this.authorId;
 	}
 	
+	/**
+	 * @return how many likes the page has
+	 */
 	public int getLikes() {
 		return this.stats.getLikes();
 	}
 	
+	/**
+	 * @return how many views the page has
+	 */
 	public int getViews() {
 		return this.stats.getViews();
 	}
 	
+	/**
+	 * @return hashset of all the viewers of the page
+	 */
+	public HashSet<Long> getViewers() {
+		return this.stats.getViewers();
+	}
+	
+	/**
+	 * @return all of the liker's ids
+	 */
 	public HashSet<Long> getLikers() {
 		return this.stats.getLikers();
 	}
 	
+	/**
+	 * @return the page's parent journal 
+	 */
 	public Journal getParentJournal() {
 		return this.parentJournal;
 	}
 	
+	/**
+	 * @return if the page has likes
+	 */
 	public boolean hasLikes() {
 		return this.hasLikes;
 	}
 	
+	/**
+	 * @return if the page has views
+	 */
 	public boolean hasViews() {
 		return this.hasViews;
 	}
 	
+	/**
+	 * @param hasLikes
+	 */
 	public void setHasLikes(boolean hasLikes) {
 		this.hasLikes = hasLikes;
 	}
 	
+	/**
+	 * @param hasViews
+	 */
 	public void setHasViews(boolean hasViews) {
 		this.hasViews = hasViews;
 	}
 	
+	/**
+	 * @param viewerId
+	 */
 	public void addView(long viewerId) {
 		this.stats.addView(viewerId);
 	}
 	
+	/**
+	 * @param likerId
+	 */
 	public void addLiker(long likerId) {
 		this.stats.addView(likerId);
 	}
 	
+	/**
+	 * @param newName
+	 */
 	public void setName(String newName) {
 		this.name = newName;
 	}
 	
+	/**
+	 * @param content
+	 */
 	public void writeContent(String content) {
 		this.content = content;
 	}
 	
+	/**
+	 * return the content as a string
+	 */
 	@Override
 	public String toString() {
 		return this.content;
--- src/myJournal/DataStructures/PageStatistics.java
+++ src/myJournal/DataStructures/PageStatistics.java
@@ -3,7 +3,6 @@ package myJournal.DataStructures;
 import java.util.HashSet;
 
 public class PageStatistics {
-	private int views;
 	private HashSet<Long> likers;
 	private HashSet<Long> viewers;
 	
@@ -11,18 +10,38 @@ public class PageStatistics {
 		
 	}
 	
+	/**
+	 * @return how many likes the page has
+	 */
 	public int getLikes() {
 		return this.likers.size();
 	}
 	
+	/**
+	 * @return how many views the page has
+	 */
 	public int getViews() {
-		return this.views;
+		return this.viewers.size();
 	}
 	
+	/**
+	 * @return a hashset of all of the viewers
+	 */
+	public HashSet<Long> getViewers() {
+		return this.viewers;
+	}
+	
+	/**
+	 * @return a hashset of all of the likers
+	 */
 	public HashSet<Long> getLikers() {
 		return likers;
 	}
 	
+	/**
+	 * add a viewer
+	 * @param viewer
+	 */
 	public void addView(long viewer) {
 		viewers.add(viewer);
 	}
