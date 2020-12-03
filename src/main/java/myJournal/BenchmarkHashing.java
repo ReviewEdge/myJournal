a57b694499a861a03b86e82071ea13be817882aa
SURRATTCB20 <SURRATTCB20@5CG024DF09.gcc.edu>
2020-12-02 19:03:23 -0500

    Messed with BenchmarkHashing a bit

--- src/main/java/myJournal/BenchmarkHashing.java
+++ src/main/java/myJournal/BenchmarkHashing.java
@@ -2,6 +2,7 @@ package myJournal;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.Base64;
 import java.util.Random;
 
 public class BenchmarkHashing {
@@ -9,10 +10,11 @@ public class BenchmarkHashing {
     public static void main(String[] args) {
         byte[] pass = new byte[12];
         Random r = new Random();
-        for(int i = 1; i < 16; i++) {
+        for(int i = 4; i < 16; i++) {
             r.nextBytes(pass);
             long then = System.nanoTime();
-            String e = BCrypt.gensalt(i);
+            String e = BCrypt.gensalt(4);
+            String f = BCrypt.hashpw(Base64.getEncoder().encodeToString(pass), e);
             long now = System.nanoTime();
             long elapsed = now - then;
             System.out.println(i + "rounds took" + (elapsed / 1000000.0) + "ms");
