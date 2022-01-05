2f23f0b6934a65e2839ffaf32ad63210cea4dce9
BROWDERKD20 <BROWDERKD20@gcc.edu>
2022-01-05 16:51:31 -0500

    Forgot allow credentials header.

--- src/main/java/myJournal/Main.java
+++ src/main/java/myJournal/Main.java
@@ -27,6 +27,7 @@ public class Main {
 				request.headers("Origin"));
 			response.header("Vary", "Origin");
 			response.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+			response.header("Access-Control-Allow-Credentials", "true");
 		});
 		options("/*",
 				Main::OptionPreflight);
